44f826c656251529d60897c742943d602ada2853
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2024-07-06 15:47:14 +0200

    Add file for extra credit exc

---
+++ extra.js
@@ -0,0 +1,169 @@
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.next = null;
+    }
+}
+
+class HashMap {
+    array = new Array(16);
+    loadFacotor = 0.75;
+
+    hash(key) {
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.array.length;
+        }
+
+        return hashCode;
+    }
+
+    resize() {
+        let entries = this.entries()
+        this.array = new Array(this.array.length * 2);
+        for (const item of entries) {
+            this.set(item[0], item[1])
+        }
+    }
+
+    set(key) {
+        let index = this.hash(key)
+        if (this.array[index] && this.array[index] instanceof Node) {
+            let currentNode = this.array[index];
+            while (currentNode.next) {
+                // overwrite value if a key exists
+                if (currentNode.key === key) {
+                    currentNode.key = key;
+                    return
+                }
+                // find LL tail
+                currentNode = currentNode.next;
+            }
+            currentNode.next = new Node(key)
+        } else {
+            this.array[index] = new Node(key)
+        }
+
+        // resize array
+        if (this.length() / this.array.length > this.loadFacotor) {
+            this.resize()
+        }
+    }
+
+    get(key) {
+        let index = this.hash(key);
+        if (this.array[index] && this.array[index] instanceof Node) {
+            let currentNode = this.array[index];
+            while (currentNode) {
+                if (currentNode.key === key) return currentNode.value;
+                currentNode = currentNode.next;
+            }
+        }
+        return null;
+    }
+
+    has(key) {
+        let index = this.hash(key);
+        if (this.array[index] && this.array[index] instanceof Node) {
+            let currentNode = this.array[index];
+            while (currentNode) {
+                if (currentNode.key === key) return true;
+                currentNode = currentNode.next;
+            }
+        }
+        return false;
+    }
+
+    remove(key) {
+        let index = this.hash(key);
+        let bucket = this.array[index];
+        if (bucket && bucket instanceof Node) {
+            let currentNode, previousNode = currentNode = bucket;
+            while (currentNode) {
+                if (currentNode.key === key) {
+                    if (previousNode === currentNode) {
+                        delete this.array[index];
+                    }
+                    else if (currentNode.next) {
+                        previousNode.next = currentNode.next;
+                    } else {
+                        previousNode.next = null;
+                    }
+                    return true;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+        }
+        return false;
+    }
+
+    length() {
+        let numberOfKeys = 0;
+        for (let i = 0; i < this.array.length; i++) {
+            let bucket = this.array[i]
+            if (bucket && bucket instanceof Node) {
+                let currentNode = bucket;
+                while (currentNode) {
+                    numberOfKeys += 1;
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return numberOfKeys;
+    }
+
+    clear() {
+        // this.array = new Array(this.array.length)
+        for (let i = 0; i < this.array.length; i++) {
+            delete this.array[i];
+        }
+    }
+
+    keys() {
+        let keys = [];
+        for (let i = 0; i < this.array.length; i++) {
+            let bucket = this.array[i]
+            if (bucket && bucket instanceof Node) {
+                let currentNode = bucket;
+                while (currentNode) {
+                    keys.push(currentNode.key);
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return keys;
+    }
+
+    values() {
+        let values = [];
+        for (let i = 0; i < this.array.length; i++) {
+            let bucket = this.array[i]
+            if (bucket && bucket instanceof Node) {
+                let currentNode = bucket;
+                while (currentNode) {
+                    values.push(currentNode.value);
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return values;
+    }
+
+    entries() {
+        let entries = [];
+        for (let i = 0; i < this.array.length; i++) {
+            let bucket = this.array[i]
+            if (bucket && bucket instanceof Node) {
+                let currentNode = bucket;
+                while (currentNode) {
+                    entries.push([currentNode.key, currentNode.value]);
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return entries;
+    }
+}
