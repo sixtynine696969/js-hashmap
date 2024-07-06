b6278009235f396e8e9f911786ce637be6e40db8
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2024-07-06 17:31:49 +0200

    Add odin tests

--- main.js
+++ main.js
@@ -37,21 +37,21 @@ class HashMap {
                 // overwrite value if a key exists
                 if (currentNode.key === key) {
                     currentNode.value = value;
-                    return
+                    return;
                 }
 
                 // find LL tail
                 if (currentNode.next === null) break;
                 currentNode = currentNode.next;
             }
-            currentNode.next = new Node(key, value)
+            currentNode.next = new Node(key, value);
         } else {
-            this.array[index] = new Node(key, value)
+            this.array[index] = new Node(key, value);
         }
 
         // resize array
         if (this.length() / this.array.length > this.loadFacotor) {
-            this.resize()
+            this.resize();
         }
     }
 
@@ -189,3 +189,47 @@ console.log(hashMap.array)
 console.log(hashMap.length())
 console.log(hashMap.keys().length)
 console.log(hashMap.array.length)
+
+// Odin tests
+let padding = '';
+for (let i = 0; i < 20; i++) {
+    padding += '-';
+}
+console.log(`${padding} ODIN TESTS ${padding}`);
+
+const test = new HashMap() // or HashMap() if using a factory
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+console.log(test.length() / test.array.length)
+console.log(test.length())
+
+test.set('lion', 'brown')
+test.set('kite', 'aqua')
+console.log(test.length())
+
+test.set('moon', 'silver')
+console.log(test.length() / test.array.length)
+console.log(test.length())
+test.set('hat', 'bubble')
+test.set('dog', 'husky')
+test.length()
+
+console.log(test.get('apple'))
+console.log(test.has('dog'))
+console.log(test.remove('lion'))
+console.log(test.length())
+console.log(test.entries())
+console.log(test.keys())
+console.log(test.values())
+test.clear()
+console.log(test.length())
